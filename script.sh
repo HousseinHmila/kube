7cfcb8f1edf5ce628e7bdc6e96f2ce4d169a8449
HousseinHmila <92945346+HousseinHmila@users.noreply.github.com>
2024-06-10 12:21:06 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -41,7 +41,7 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 # Step 4: Initialize Cluster with kubeadm (Only master node)
-kubeadm init --pod-network-cidr=10.244.0.0/16 
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16 
 mkdir -p \$HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf \$HOME/.kube/config
 sudo chown \$(id -u):\$(id -g) \$HOME/.kube/config
