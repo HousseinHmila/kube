48b1dae2178dc020561b099755c0e36e3effca63
HousseinHmila <92945346+HousseinHmila@users.noreply.github.com>
2024-06-12 19:24:18 +0100

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,6 +1,6 @@
 # Step 4: Initialize Cluster with kubeadm (Only master node)
 
-sudo kubeadm init --apiserver-advertise-address=${PUBLIC_IP} --pod-network-cidr=10.244.0.0/16
+sudo kubeadm init --control-plane-endpoint="${PUBLIC_IP}:6443" --pod-network-cidr=10.244.0.0/16
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
