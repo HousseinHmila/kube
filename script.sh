e5d74eac43609ca6dfbdb24c93fcde646938f1c7
HousseinHmila <92945346+HousseinHmila@users.noreply.github.com>
2024-06-12 19:01:17 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -45,18 +45,4 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
-# Step 4: Initialize Cluster with kubeadm (Only master node)
-PRIVATE_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-
-sudo kubeadm init --apiserver-advertise-address=${PRIVATE_IP} --pod-network-cidr=10.244.0.0/16
-
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-# Step 5: Install Network Addon (flannel) (master node)
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-
-alias k=kubectl
-k get node
 
