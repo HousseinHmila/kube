6b16f9c43aff1203e5e3ba9acb62728d1e10b421
HousseinHmila <92945346+HousseinHmila@users.noreply.github.com>
2024-06-13 10:21:22 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -45,4 +45,16 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
+sudo kubeadm init --control-plane-endpoint="${PUBLIC_IP}:6443" --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Step 5: Install Network Addon (flannel) (master node)
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
+alias k=kubectl
+k get node
+
 
